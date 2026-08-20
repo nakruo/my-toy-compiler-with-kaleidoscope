26cb20596d981391dd59a152498277495819d83e
nakruo <furkanaliileri@gmail.com>
2026-08-20 14:12:28 +0300

    Kaleidoscope chapter 1 creating lexer

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+enum Token 
+{
+    tok_eof = -1,
+
+    tok_def = -2,
+    tok_extern = -3,
+
+    tok_identifier = -4,
+    tok_number = -5,
+};
+static std::string IdentifierStr;
+static double NumVal;
+
+static int gettok()
+{
+    static int LastChar = ' ';
+
+    while (isspace(LastChar)) LastChar = getchar();
+}
+
+int main()
+{
+
+}
