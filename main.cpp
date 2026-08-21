88059b64de25996e20d3cfd87c74657fd6ae2f88
nakruo <furkanaliileri@gmail.com>
2026-08-21 16:27:13 +0300

    Add AST Classes

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <memory>
+#include <vector>
+
 
 enum Token 
 {
@@ -58,7 +61,74 @@ static int gettok()
     return ThisChar;
 }
 
+class ExprAST 
+{
+public:
+    virtual ~ExprAST() = default;
+};
+
+class NumberExprAST : public ExprAST 
+{
+    double Val;
+
+public:
+    NumberExprAST(double Val) : Val(Val) {}
+};
+
+class VariableExprAST : public ExprAST 
+{
+    std::string Name;
+
+public:
+    VariableExprAST(const std::string &Name) : Name(Name) {}
+};
+
+class BinaryExprAST : public ExprAST
+{
+    char OP;
+    std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+    BinaryExprAST(char OP, std::unique_ptr<ExprAST> LHS, std::unique_ptr<ExprAST> RHS)
+        : OP(OP), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+class CallExprAST : public ExprAST
+{
+    std::string Callee;
+    std::vector<std::unique_ptr<ExprAST>> Args;
+
+
+public:
+    CallExprAST(const std::string &Callee, std::vector<std::unique_ptr<ExprAST>> Args)
+        :Callee(Callee), Args(std::move(Args)) {}
+};
+
+class PrototypeAST
+{
+    std::string Name;
+    std::vector<std::string> Args;
+
+public:
+    PrototypeAST(const std::string &Name, std::vector<std::string> Args)
+        :Name(Name), Args(std::move(Args)) {}
+
+    const std::string &getName() const { return Name; }
+
+};
+
+class FunctionAST 
+{
+    std::unique_ptr<PrototypeAST> Proto;
+    std::unique_ptr<ExprAST> Body;
+
+public: 
+    FunctionAST(std::unique_ptr<PrototypeAST> Proto, std::unique_ptr<ExprAST> Body)
+        : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
+
+
 int main()
 {
-
+    return 0;
 }
