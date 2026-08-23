d2bbe8c6c03cf93d91b5a2bfdf88f4958ca9ee55
nakruo <furkanaliileri@gmail.com>
2026-08-23 14:51:02 +0300

    Add basic expression parsing and error handling

--- main.cpp
+++ main.cpp
@@ -127,6 +127,40 @@ public:
         : Proto(std::move(Proto)), Body(std::move(Body)) {}
 };
 
+static int CurTok;
+static int getNextTOken() 
+{
+    return CurTok = gettok();
+}
+
+std::unique_ptr<ExprAST> LogError(const char *Str)
+{
+    fprintf(stderr, "Error: %s\n", Str);
+    return nullptr;
+}
+
+std::unique_ptr<PrototypeAST> LogErrorP(const char *Str)
+{
+    LogError(Str);
+    return nullptr;
+}
+
+static std::unique_ptr<ExprAST> ParseNumberEXpr()
+{
+    auto Result = std::make_unique<NumberExprAST>(NumVal);
+    getNextTOken();
+    return std::move(Result);
+}
+
+
+static std::unique_ptr<ExprAST> ParseExpression();
+static std::unique_ptr<ExprAST> ParseParenExpr() 
+{
+    getNextTOken();
+    auto V = ParseExpression();
+}
+
+
 
 int main()
 {
