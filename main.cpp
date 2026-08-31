e83926bc4e41b533b53cd27fa7635060ade0bedf
nakruo <furkanaliileri@gmail.com>
2026-08-31 15:32:46 +0300

    Ch 4.4 complete: Full JIT integration, persistant memory and C++ sdlib linking

--- main.cpp
+++ main.cpp
@@ -32,6 +32,18 @@
 #include "llvm/Transforms/Scalar/Reassociate.h"
 #include "llvm/Transforms/Scalar/SimplifyCFG.h"
 
+#ifdef _WIN32
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT
+#endif
+
+extern "C" DLLEXPORT double putchard(double X)
+{
+    fputc((char)X, stderr);
+    return 0;
+}
+
 
 
 using namespace llvm;
